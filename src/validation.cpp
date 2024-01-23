3f2eabe10fdcb088ea7885d797e9a95ffd5a8129
borshchevik <85487261+borshchevik@users.noreply.github.com>
2024-01-23 17:50:54 +1100

    rollback logo

--- src/validation.cpp
+++ src/validation.cpp
@@ -3568,7 +3568,7 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
     //only start checking this error after block 5000 and only on testnet and mainnet, not regtest
     if(pindex->nHeight > 5000 && !m_params.MineBlocksOnDemand()) {
         //sanity check in case an exploit happens that allows new coins to be minted
-        if(pindex->nMoneySupply > (uint64_t)(100000000 + ((pindex->nHeight - 5000) * 4)) * COIN){
+        if(pindex->nMoneySupply > (uint64_t)(1000000000 + ((pindex->nHeight - 5000) * 4)) * COIN){
             return state.Invalid(BlockValidationResult::BLOCK_CONSENSUS, "incorrect-money-supply", "ConnectBlock(): Unknown error caused actual money supply to exceed expected money supply");
         }
     }
